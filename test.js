874252f00ad8fdc555b9810ca275bffe7564c75b
Jannis R <mail@jannisr.de>
2018-01-11 21:33:03 +0100

    tests :white_check_mark:

--- test.js
+++ test.js
@@ -1,8 +1,120 @@
 'use strict'
 
-const test = require('tape')
+const pTape = require('tape-promise').default
+const tape = require('tape')
+const isStream = require('is-stream')
 
-const todo = require('.')
+const test = pTape(tape)
 
-test('todo', (t) => {
+const all = require('.')
+const getCapabilities = require('./get-capabilities')
+const getFeatures = require('./get-features')
+
+const endpoint = 'https://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_postleit'
+const layer = 'fis:re_postleit'
+const bbox = [387000, 5812000, 386000, 5813000]
+
+const isObj = o => 'object' === typeof o && !Array.isArray(o)
+
+test('getCapabilities', async (t) => {
+	t.equal(all.getCapabilities, getCapabilities)
+
+	const caps = await getCapabilities(endpoint)
+	t.ok(caps)
+
+	t.ok(Array.isArray(caps.operations))
+	for (let op of caps.operations) {
+		t.ok(op)
+
+		t.equal('string', typeof op.name)
+		t.ok(op.name)
+
+		t.ok(Array.isArray(op.params))
+		for (let param of op.params) {
+			t.ok(param)
+
+			t.equal('string', typeof param.name)
+			t.ok(param.name)
+
+			if (param.defaultValue !== null) {
+				t.equal('string', typeof param.defaultValue)
+				t.ok(param.defaultValue)
+			}
+
+			if (param.allowedValues !== null) {
+				t.ok(Array.isArray(param.allowedValues))
+				for (let val of param.allowedValues) {
+					t.equal('string', typeof val)
+					t.ok(val)
+				}
+			}
+		}
+	}
+
+	if (caps.defaultVersion !== null) {
+		t.equal('string', typeof caps.defaultVersion)
+		t.ok(caps.defaultVersion)
+	}
+	if (caps.allowedVersions !== null) {
+		t.ok(Array.isArray(caps.allowedVersions))
+		for (let version of caps.allowedVersions) {
+			t.equal('string', typeof version)
+			t.ok(version)
+		}
+	}
+
+	t.ok(Array.isArray(caps.featureTypes))
+	for (let type of caps.featureTypes) {
+		t.ok(type)
+
+		t.equal('string', typeof type.name)
+		t.ok(type.name)
+		t.equal('string', typeof type.title)
+		t.equal('string', typeof type.description)
+		t.equal('string', typeof type.crs)
+		t.ok(type.crs)
+
+		if (type.outputFormats !== null) {
+			t.ok(Array.isArray(type.outputFormats))
+			for (let format of type.outputFormats) {
+				t.equal('string', typeof format)
+				t.ok(format)
+			}
+		}
+
+		t.ok(isObj(type.bbox))
+		if (isObj(type.bbox)) {
+			t.equal('number', typeof type.bbox.minLat)
+			t.equal('number', typeof type.bbox.minLon)
+			t.equal('number', typeof type.bbox.maxLat)
+			t.equal('number', typeof type.bbox.maxLon)
+		}
+	}
+
+	t.ok(Array.isArray(caps.spatialCapabilities))
+	for (let cap of caps.spatialCapabilities) {
+		t.equal('string', typeof cap)
+		t.ok(cap)
+	}
+
+	t.end()
+})
+
+test('getFeatures', async (t) => {
+	t.equal(all.getFeatures, getFeatures)
+
+	const features = getFeatures(endpoint, layer, {bbox})
+	t.ok(isStream.readable(features))
+
+	features.on('data', (feature) => {
+		t.ok(feature)
+		// todo
+	})
+
+	await new Promise((resolve, reject) => {
+		features.once('error', reject)
+		features.once('end', resolve)
+	})
+
+	t.end()
 })
