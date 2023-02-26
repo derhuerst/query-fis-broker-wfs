cbba333422d5534ba9a9b93c4ace0bf652d421e3
Jannis R <mail@jannisr.de>
2023-02-26 22:23:25 +0100

    adapt tests to latest data ✅, let CI run the tests 💚

--- test.js
+++ test.js
@@ -10,8 +10,8 @@ const all = require('.')
 const getCapabilities = require('./get-capabilities')
 const getFeatures = require('./get-features')
 
-const endpoint = 'https://fbinter.stadt-berlin.de/fb/wfs/geometry/senstadt/re_postleit'
-const layer = 'fis:re_postleit'
+const endpoint = 'https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_plz'
+const layer = 'fis:s_plz'
 const bbox = [387000, 5812000, 386000, 5813000]
 
 const isObj = o => 'object' === typeof o && !Array.isArray(o)
